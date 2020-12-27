13d8857bc2313b1052f1edcd897631596837ea65
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2020-12-27 20:12:29 +0900

    Create E.cpp

---
+++ OpenContest/2020천하제일코딩대회/E.cpp
@@ -0,0 +1,68 @@
+//================code===================//
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+#define ci(t) cin>>t
+#define co(t) cout<<t
+#define LL long long
+#define fa(i,a,b) for(int i=a;i<b;++i)
+#define fd(i,a,b) for(int i=a;i>b;--i)
+#define setp pair<pair<int,int>,int>
+#define setl pair<int,int>
+#define M_PI 3.14159265358979323846
+#define micro 0.000001
+
+#ifdef OHSOLUTION
+#define ce(t) cerr<<t
+#define AT cerr << "\n=================ANS=================\n";
+#define AE cerr << "\n=====================================\n";
+#else
+#define AT
+#define AE
+#define ce(t)
+#endif
+
+using namespace std;
+
+LL gcd(LL a, LL b) { return a % b ? gcd(b, a % b) : b; }
+LL lcm(LL a, LL b) { return (a * b) / gcd(a, b); }
+
+pair <int, int> vu[8] = { {0,1},{-1,0},{0,-1} ,{1,0},{0,1}, {0,-1} , {1,0},{-1,0} }; //RLDU EWSN
+
+template<typename T, typename U>
+void ckmax(T& a, U b) { a = a < b ? b : a; }
+template<typename T, typename U>
+void ckmin(T& a, U b) { a = a > b ? b : a; }
+struct gcmp { bool operator()(LL a, LL b) { return a < b; } };
+struct lcmp { bool operator()(LL a, LL b) { return a > b; } };
+
+const int max_v = 2e3 + 7;
+const int INF = 1e9 + 7;
+const LL LNF = 1e18 + 7;
+const LL mod = 1e9 + 7;
+
+LL dp[max_v][3];
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int n; ci(n);
+	dp[1][2] = 1;
+	if (n == 1)
+	{
+		co(0);
+		return 0;
+	}
+
+	fa(i, 2, n + 1)
+	{
+		dp[i][0] = (dp[i - 1][2] + dp[i - 1][1])%mod;
+		dp[i][1] = (dp[i - 1][2] + dp[i - 1][0]) % mod;
+		dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
+	}
+
+
+	co(dp[n][0]%mod);
+
+	return 0;
+}
