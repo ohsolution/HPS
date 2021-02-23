086325dfc27ca77ca53cb28d2e038b4bc90570ec
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-02-23 21:30:50 +0900

    Create C.cpp

---
+++ CodeForce/div2-704/C.cpp
@@ -0,0 +1,97 @@
+
+//================code===================//
+
+//#define TLE
+
+#ifdef TLE
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <ctime>
+
+#define ci(t) cin>>t
+#define co(t) cout<<t
+#define LL long long
+#define ld long double
+#define fa(i,a,b) for(LL i=(a);i<(LL)(b);++i)
+#define fd(i,a,b) for(LL i=(a);i>(LL)(b);--i)
+#define setp pair<pair<int,int>,int>
+#define setl pair<LL,LL>
+
+#define M_PI 3.14159265358979323846
+#define micro 0.000001
+
+using namespace std;
+
+#ifdef OHSOLUTION
+#define ce(t) cerr<<t
+#define AT cerr << "\n=================ANS=================\n"
+#define AE cerr << "\n=====================================\n"
+LL gcd(LL a, LL b) { return a % b ? gcd(b, a % b) : b; }
+LL lcm(LL a, LL b) { return (a * b) / gcd(a, b); }
+#else
+#define AT
+#define AE
+#define ce(t)
+#define __popcnt __builtin_popcount
+#define gcd __gcd
+#define lcm __lcm
+#endif
+
+
+pair <int, int> vu[9] = { {0,1},{1,0},{0,-1} ,{-1,0},{0,1},{1,0}, {-1,1} , {1,-1},{-1,-1} }; //RDLU EWSN
+
+template<typename T, typename U> void ckmax(T& a, U b) { a = a < b ? b : a; }
+template<typename T, typename U> void ckmin(T& a, U b) { a = a > b ? b : a; }
+struct gcmp { bool operator()(LL a, LL b) { return a < b; } bool operator()(setl a, setl b) { return a.second < b.second; } };
+struct lcmp { bool operator()(LL a, LL b) { return a > b; } bool operator()(setl a, setl b) { return a.second > b.second; } };
+
+const int max_v = 2e5 + 7;
+const int INF = 1e9 + 7;
+const LL LNF = (LL)5e18 + 7ll;
+const LL mod = 998244353;
+
+int dp[max_v];
+
+int main()
+{
+#ifdef OHSOLUTION
+	freopen("input.txt", "r", stdin);
+#endif	
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);				
+	int n, m; ci(n >> m);
+	string s, t;
+	ci(s >> t);
+
+
+	int base = n - 1;
+	fd(i, m - 1, -1)
+	{
+		while (t[i] != s[base]) --base;
+		dp[i] = base;
+		--base;
+	}
+
+	//assert(base >= -1);
+
+	base = 0;
+	while (t[0] != s[base]) ++base;
+	++base;
+	LL ans = 0;
+
+	fa(i, 1, m)
+	{
+		ckmax(ans, dp[i] - base + 1);
+		while (t[i] != s[base]) ++base;
+		++base;
+	}
+
+	co(ans);
+	return 0;
+}
