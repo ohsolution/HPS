01372e5e6f7792826bbbb13776c4018da949a8d9
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-03-02 14:11:34 +0900

    Create A.cpp

---
+++ OpenContest/SUAPC 2021 Winter/A.cpp
@@ -0,0 +1,209 @@
+//================code===================//
+
+#define TLE
+
+#ifdef TLE
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <ctime>
+
+#define ci(t) cin>>t
+#define co(t) cout<<t
+#define LL long long
+#define ld long double
+#define fa(i,a,b) for(LL i=(a);i<(LL)(b);++i)
+#define fd(i,a,b) for(LL i=(a);i>(LL)(b);--i)
+#define setp pair<pair<int,int>,int>
+#define setl pair<LL,LL>
+
+#define M_PI 3.14159265358979323846	
+#define micro 0.000001
+
+using namespace std;
+
+#ifdef OHSOLUTION
+#define ce(t) cerr<<t
+#define AT cerr << "\n=================ANS=================\n"
+#define AE cerr << "\n=====================================\n"
+LL gcd(LL a, LL b) { return a % b ? gcd(b, a % b) : b; }
+LL lcm(LL a, LL b) { return (a * b) / gcd(a, b); }
+#else
+#define AT
+#define AE
+#define ce(t)
+#define __popcnt __builtin_popcountll
+#define gcd __gcd
+#define lcm __lcm
+#endif
+
+
+pair <int, int> vu[9] = { {0,1},{1,0},{0,-1} ,{-1,0},{0,1},{1,0}, {-1,1} , {1,-1},{-1,-1} }; //RDLU EWSN
+
+template<typename T, typename U> void ckmax(T& a, U b) { a = a < b ? b : a; }
+template<typename T, typename U> void ckmin(T& a, U b) { a = a > b ? b : a; }
+struct gcmp { bool operator()(LL a, LL b) { return a < b; } bool operator()(setl a, setl b) { return a.second < b.second; } };
+struct lcmp { bool operator()(LL a, LL b) { return a > b; } bool operator()(setl a, setl b) { return a.second > b.second; } };
+
+const int max_v = 2e5 + 7;
+const int INF = 1e9 + 7;
+const LL LNF = (LL)5e18 + 7ll;
+const LL mod = 1e9 + 7;
+
+unordered_map<char, int> xh = { {'+',0},{'-',0},{'/',1},{'*',1} };
+struct exec
+{
+	LL a, b;
+	char op;
+	LL val;
+	int t;
+	int idx;
+
+	void cal()
+	{
+		if (op == '+') val = a + b;
+		else if (op == '-') val = a - b;
+		else if (op == '*') val = a * b;
+		else if(op=='/') val = a / b;
+	}
+};
+
+struct cmp
+{
+	bool operator()(exec& x, exec& y)
+	{
+		if (x.val == y.val)
+		{
+			if (xh[x.op] == xh[y.op]) return x.idx > y.idx;
+			else return xh[x.op] < xh[y.op];
+		}
+
+		return x.val < y.val;
+	}
+};
+
+priority_queue<exec, vector<exec>, cmp> pq;
+
+bool vist[max_v];
+int p[max_v][2];
+
+int find(int idx,bool rt)
+{
+	return ~p[idx][rt] ? p[idx][rt] = find(p[idx][rt],rt) : idx;
+}
+
+void merge(int a, int b, bool rt)
+{
+	a = find(a, rt);
+	b = find(b, rt);
+
+	if (a == b) return;
+	
+	p[a][rt] = b;
+}
+
+vector <exec> vi;
+
+int main()
+{
+#ifdef OHSOLUTION
+	freopen("input.txt", "r", stdin);
+#endif	
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);	
+	string s; ci(s);
+	memset(p, -1, sizeof(p));
+	string pre = "";
+	string nxt = "";
+	int sidx = 0;
+
+	assert(s[0] >= '0' && s[0] <= '9');
+
+	while ((s[sidx] <= '9' && s[sidx] >= '0')) pre += s[sidx++];
+
+	if (sidx >= s.size())
+	{
+		co(stoll(pre));
+		return 0;
+	}
+
+	int cnt = 0;
+
+	fa(i, sidx, s.size())
+	{
+		char op = s[i];
+		fa(j, i + 1, s.size())
+		{
+			if (s[j] <= '9' && s[j] >= '0') nxt += s[j];
+			else break;
+
+			i = j;
+		}
+		exec tmp;
+
+		tmp.a = stoll(pre);
+		tmp.b = stoll(nxt);
+		tmp.t = 0;
+		tmp.idx = cnt++;
+		tmp.op = op;		
+		tmp.cal();
+
+		pq.push(tmp);
+		vi.push_back(tmp);
+
+		pre = nxt;
+		nxt.clear();
+	}
+
+	LL ans = 0;
+	while (!pq.empty())
+	{
+		exec cur;
+		do
+		{
+			cur = pq.top(); pq.pop();
+		} while (pq.size() && vist[cur.idx]);
+
+		if (vi[cur.idx].t > cur.t) continue;
+
+		if (vist[cur.idx]) break;
+		vist[cur.idx] = 1;
+
+		//ce(cur.idx << " : " << cur.a << " " << cur.op << " " << cur.b << " = " << cur.val << "T : " << cur.t << endl);
+
+		ans = cur.val;
+
+		int rpos = find(cur.idx, 1) + 1;
+		int lpos = find(cur.idx, 0) - 1;
+
+		
+		if (lpos>=0)
+		{
+			vi[lpos].b = cur.val;
+			vi[lpos].cal();
+			++vi[lpos].t;
+			pq.push(vi[lpos]);
+		}
+
+		if (rpos < vi.size())
+		{
+			vi[rpos].a= cur.val;
+			vi[rpos].cal();
+			++vi[rpos].t;
+			pq.push(vi[rpos]);
+		}
+
+		if (rpos < vi.size()) merge(rpos, cur.idx,0);
+		if (lpos >= 0) merge(lpos, cur.idx, 1);
+
+	}
+
+	co(ans);
+
+	return 0;
+}
