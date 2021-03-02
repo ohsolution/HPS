2cb9a2b8c9187c44f6463699d58f8d70fdb4dbb1
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-03-02 14:14:49 +0900

    Create K.cpp

---
+++ OpenContest/SUAPC 2021 Winter/K.cpp
@@ -0,0 +1,102 @@
+//================code===================//
+
+#define TLE
+
+#ifdef TLE
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <ctime>
+
+#define ci(t) cin>>t
+#define co(t) cout<<t
+#define LL long long
+#define ld long double
+#define fa(i,a,b) for(LL i=(a);i<(LL)(b);++i)
+#define fd(i,a,b) for(LL i=(a);i>(LL)(b);--i)
+#define setp pair<pair<int,int>,int>
+#define setl pair<LL,LL>
+
+#define M_PI 3.14159265358979323846	
+#define micro 0.000001
+
+using namespace std;
+
+#ifdef OHSOLUTION
+#define ce(t) cerr<<t
+#define AT cerr << "\n=================ANS=================\n"
+#define AE cerr << "\n=====================================\n"
+LL gcd(LL a, LL b) { return a % b ? gcd(b, a % b) : b; }
+LL lcm(LL a, LL b) { return (a * b) / gcd(a, b); }
+#else
+#define AT
+#define AE
+#define ce(t)
+#define __popcnt __builtin_popcountll
+#define gcd __gcd
+#define lcm __lcm
+#endif
+
+
+pair <int, int> vu[9] = { {0,1},{1,0},{0,-1} ,{-1,0},{0,1},{1,0}, {-1,1} , {1,-1},{-1,-1} }; //RDLU EWSN
+
+template<typename T, typename U> void ckmax(T& a, U b) { a = a < b ? b : a; }
+template<typename T, typename U> void ckmin(T& a, U b) { a = a > b ? b : a; }
+struct gcmp { bool operator()(LL a, LL b) { return a < b; } bool operator()(setl a, setl b) { return a.second < b.second; } };
+struct lcmp { bool operator()(LL a, LL b) { return a > b; } bool operator()(setl a, setl b) { return a.second > b.second; } };
+
+const int max_v = 2.5e3 + 7;
+const int INF = 1e9 + 7;
+const LL LNF = (LL)5e18 + 7ll;
+const LL mod = 1e9 + 7;
+
+map<LL, int> xh;
+
+int main()
+{
+#ifdef OHSOLUTION
+	freopen("input.txt", "r", stdin);
+#endif	
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	LL n; ci(n);
+
+	for (LL i = 2; i * i <= n; ++i)
+	{
+		while (n % i == 0)
+		{
+			++xh[i];
+			n /= i;
+		}
+	}
+
+	if (n != 1) ++xh[n];
+
+	vector <LL> ans;
+	for (auto& x : xh) while (x.second--) ans.push_back(x.first);
+
+	if (ans.size() < 2) co(-1);
+	else
+	{
+		if (ans.size() & 1)
+		{
+			LL rb = 1;
+			int c = 3;
+			while (c--) rb *= ans.back(),ans.pop_back();
+			for (int i = 0; i < ans.size(); i += 2) co(ans[i] * ans[i + 1] << " ");
+			co(rb);
+		}
+		else
+		{
+			for (int i = 0; i < ans.size(); i += 2) co(ans[i] * ans[i + 1] << " ");
+		}
+	}
+
+
+	return 0;
+}
