c4320ac41d173b4acd412f8756105a1020d3e0dc
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2021-03-07 01:59:38 +0900

    Create B.cpp

---
+++ CodeForce/div2-705/B.cpp
@@ -0,0 +1,91 @@
+
+//================code===================//
+
+//#define TLE
+
+#ifdef TLE
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <ctime>
+
+#define ci(t) cin>>t
+#define co(t) cout<<t
+#define LL long long
+#define ULL LL
+#define ld long double
+#define fa(i,a,b) for(LL i=(a);i<(LL)(b);++i)
+#define fd(i,a,b) for(LL i=(a);i>(LL)(b);--i)
+#define setp pair<pair<int,int>,int>
+#define setl pair<LL,LL>
+#define micro 0.000001
+
+using namespace std;
+
+#ifdef OHSOLUTION
+#define ce(t) cerr<<t
+#define AT cerr << "\n=================ANS=================\n"
+#define AE cerr << "\n=====================================\n"
+LL gcd(LL a, LL b) { return a % b ? gcd(b, a % b) : b; }
+LL lcm(LL a, LL b) { return (a * b) / gcd(a, b); }
+#else
+#define AT
+#define AE
+#define ce(t)
+#define __popcnt __builtin_popcountll
+#define gcd __gcd
+#define lcm __lcm
+#endif
+
+
+
+
+pair <int, int> vu[9] = { {0,1},{1,0},{0,-1} ,{-1,0},{0,0},{1,0}, {-1,1} , {1,-1},{-1,-1} }; //RDLU EWSN
+
+template<typename T, typename U> void ckmax(T& a, U b) { a = a < b ? b : a; }
+template<typename T, typename U> void ckmin(T& a, U b) { a = a > b ? b : a; }
+struct gcmp { bool operator()(LL a, LL b) { return a < b; } bool operator()(setl a, setl b) { return a.second < b.second; } };
+struct lcmp { bool operator()(LL a, LL b) { return a > b; } bool operator()(setl a, setl b) { return a.second > b.second; } };
+
+const int max_v = 2e5 + 7;
+const int INF = 1e9 + 7;
+const LL LNF = (LL)5e18 + 7ll;
+const LL mod = 1e9 + 7;
+
+int main()
+{
+#ifdef OHSOLUTION
+	freopen("input.txt", "r", stdin);
+#endif	
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);	
+	int cnt; ci(cnt);
+	while (cnt--)
+	{
+		int n, k;
+
+		ci(n >> k);
+
+		vector <LL> ans;
+
+		fa(i, k + 1, n + 1) ans.push_back(i);
+
+		int t = ceil(k/2.0);
+
+		fa(i, t, k) ans.push_back(i);
+
+		co(ans.size() << "\n");
+
+		sort(ans.begin(), ans.end());
+		
+		for (auto x : ans) co(x << " ");
+		co("\n");
+
+	}
+	return 0;
+}
